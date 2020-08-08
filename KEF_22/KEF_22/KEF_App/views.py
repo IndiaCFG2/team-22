f95d19b9ff51bca6e3955671d1a5676558d43cb8
nand16 <54791875+nand16@users.noreply.github.com>
2020-08-08 22:35:34 +0530

    Update views.py

--- KEF_22/KEF_22/KEF_App/views.py
+++ KEF_22/KEF_22/KEF_App/views.py
@@ -19,3 +19,31 @@ def secret_page(request):
 
 class SecretPage(LoginRequiredMixin, TemplateView):
     template_name = 'secret_page.html'
+
+def cal_score():
+    if method.request == "POST":
+        path = request.POST.get('file_csv')
+        l = csv.DictReader(open(path))
+        def add_scores(email, score, name):
+            try:
+                obj1 = Teacher.objects.get(teacher_email=email)
+                obj3 = Assesment.objects.get(assesment_name=name)
+                obj2 = Assesment_Teacher.objects.get_or_create(test_id=obj3.id, teacher_id=obj1.id, test_score=score)
+                obj2.save()
+            except Assesment_Teacher.DoesNotExist:
+                pass
+        for i in l:
+            for j in i:
+                if j == 'Timestamp':
+                    date = i[j][:8]
+                if j=='Email Address':
+                    name = i[j]
+                if j == 'Score':
+                    s=''
+                    for k in range(len(i[j])):
+                        if i[j][k]=='/':
+                            break
+                        else:
+                            s+=i[j][k]
+
+                    add_scores(email, int(s[:-1]), name_test)
