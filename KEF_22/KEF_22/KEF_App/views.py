063f7dcacb2c4ee558758d1e2aaee133b21f0127
nand16 <54791875+nand16@users.noreply.github.com>
2020-08-09 02:43:25 +0530

    Update views.py

--- KEF_22/KEF_22/KEF_App/views.py
+++ KEF_22/KEF_22/KEF_App/views.py
@@ -47,3 +47,61 @@ def cal_score():
                             s+=i[j][k]
 
                     add_scores(email, int(s[:-1]), name_test)
+
+def visuals_teacher(request):
+    l1 = Assesment_Teacher.objects.values_list('teacher_id','scores')
+    scores = []
+    current_user = request.user
+    id = current_user.id
+    for i in range(len(l1)):
+        if l1[i][0] == id:
+            scores.append(l1[i][1])
+
+    plt.plot(scores,'g',label='line one',linewidth=2)
+    plt.title("Overall progress of teacher")
+    plt.xlabel("No. of Tests")
+    plt.ylabel("Scores")
+    plt.legend()
+    plt.show()
+    plt.savefig('static/plot1.png')
+    return render(request,'visuals_teacher.html')
+
+def visuals_school(request):
+    l1 = Assesment_Teacher.objects.values_list('teacher_id','scores')
+    l2 = School_Teacher.objects.values_list('teacher_id','school_id')
+    current_school = request.user
+    id = current_school.id
+    d = {}
+    for i in range(len(l2)):
+        if l2[i][1] == id:
+            d[l2[i][0]]=[]
+
+    for i in range(len(l1)):
+        if l1[i][0] in d:
+            d[l1[i][0]].append(l1[i][1])
+
+    c=0
+    for f in d:
+        x = [i for i in range(1,len(d)+1)]
+        plt.plot(x,d[f])
+        plt.title("Overall progress of teacher in quiz",f)
+        plt.xlabel("No. of Teachers")
+        plt.ylabel("Scores")
+        plt.legend()
+        plt.show()
+        plt.savefig('plot_school',c,'.png')
+        c+=1
+
+    cumm_sum = []
+    for i in d:
+        cumm_sum.append(sum(d[i])//len(d[i]))
+    x = [i for i in range(1,len(d)+1)]
+    plt.plot(x,cumm_sum)
+    plt.plot(x,d[f])
+    plt.title("Overall cummulative progress of teacher in school")
+    plt.xlabel("No. of Teachers")
+    plt.ylabel("Scores")
+    plt.legend()
+    plt.show()
+    plt.savefig('plot_school.png')
+    return render(request,'visuals_school.html')
